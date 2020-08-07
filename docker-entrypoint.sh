f14016e42f53b3bdd278ef1fb0ffb3a0b4e83d25
Léo Depriester <leo.depriester@camptocamp.com>
2020-08-07 10:46:03 +0200

    fix cattle_dynamic_path metrics

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,17 +9,26 @@ if [ "$CATTLE_DYNAMIC_PATH" = true ] && [ ! -z "${VOLUME_NAME}" ]; then
   dynamic_path="${DESTINATION_BUCKET}/${instance}/${VOLUME_NAME}"
   echo "Computed dynamic path: ${dynamic_path}"
   /usr/bin/rclone "$@" dst:$dynamic_path |& tee rclone.log
+
+  src=${VOLUME_NAME}
+  dst=${DESTINATION_BUCKET}
 else
   /usr/bin/rclone "$@" |& tee rclone.log
+
+  src=$(cat /proc/1/cmdline|strings|grep ^src:|cut -d: -f2 | sed -r 's/\//_/g')
+  dst=$(cat /proc/1/cmdline|strings|grep ^dst:|cut -d: -f2 | sed -r 's/\//_/g')
 fi
 
 if [ ! -z "${PUSHGATEWAY_URL}" ]; then
   echo "Sending metrics to ${PUSHGATEWAY_URL}"
 
-  if getent hosts rancher-metadata >/dev/null; then
-    instance=$(curl http://rancher-metadata/latest/self/container/name)
-  else
-    instance=$(hostname -f)
+
+  if [ ! "$CATTLE_DYNAMIC_PATH" = true ]; then
+    if getent hosts rancher-metadata >/dev/null; then
+      instance=$(curl http://rancher-metadata/latest/self/container/name)
+    else
+      instance=$(hostname -f)
+    fi
   fi
 
   transferred_raw=$(sed -n '/Transferred: *\([^ ]\+\) \([A-Za-z]\?\)Bytes.*/ s//\1\2/p' rclone.log | tail -n1)
@@ -27,10 +36,7 @@ if [ ! -z "${PUSHGATEWAY_URL}" ]; then
   errors=$(sed -n '/Errors: */ s///p' rclone.log | tail -n1)
   checks=$(sed -n '/Checks: */ s///p' rclone.log | tail -n1)
 
-  src=$(cat /proc/1/cmdline|strings|grep ^src:|cut -d: -f2 | sed -r 's/\//_/g')
-  dst=$(cat /proc/1/cmdline|strings|grep ^dst:|cut -d: -f2 | sed -r 's/\//_/g')
-
-  cat <<EOF | curl -s --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/rclone/source/${src}/destination/${dst}"
+  cat <<EOF | curl -s --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/rclone/source/${src}/destination/${dst}/instance/${instance}"
 # TYPE rclone_transferred_bytes gauge
 rclone_transferred_bytes ${transferred}
 # TYPE rclone_errors gauge
